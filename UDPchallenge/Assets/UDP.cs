c02351d151eb9a68bdc6f7baf946502f9c56136d
Arata Yasuhara <yasuharaarata@vogue.is.uec.ac.jp>
2018-11-22 09:57:08 +0900

    UDPconnect
    UDPconnect use CHAR

---
+++ UDPchallenge/Assets/UDP.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+public class UDP : MonoBehaviour
+{
+
+    static UdpClient udp;
+    IPEndPoint remoteEP = null;
+    int i = 0;
+    // Use this for initialization
+    void Start()
+    {
+        int LOCA_LPORT = 50007;
+
+        udp = new UdpClient(LOCA_LPORT);
+        udp.Client.ReceiveTimeout = 2000;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        IPEndPoint remoteEP = null;
+        byte[] data = udp.Receive(ref remoteEP);
+        string text = Encoding.UTF8.GetString(data);
+        Debug.Log(text);
+        Thread.Sleep(1000);
+    }
+}
